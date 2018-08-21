aedc2b1b94d507773be9e5794050516703e4d556
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-21 08:47:42 -0700

    Agregar archivos de proyecto.

---
+++ Colecciones/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Alumno alumno = new Alumno();
+            alumno.Nombre = "Rubén Bojórquez";
+            alumno.Matricula = "179051";
+
+            Materia matematicas = new Materia();
+            matematicas.Nombre = "Matematicas";
+            matematicas.Identificador = "MT123458";
+
+            alumno.Materias.Add(new Materia());
+            alumno.Materias.Add(matematicas);
+
+            alumno.Materias[0].Nombre = "Historia";
+            alumno.Materias[0].Identificador = "HT75265";
+
+            alumno.Materias.Add(new Materia());
+            alumno.Materias[2].Nombre = "Español";
+            alumno.Materias[2].Identificador = "ES852";
+
+            //alumno.Materias.RemoveAt(1);
+            //alumno.Materias.RemoveAt(1);
+            //alumno.Materias.Clear();
+
+            Console.WriteLine("Alumno: " + alumno.Nombre);
+            Console.WriteLine("Numero de materias: " + alumno.Materias.Count);
+            foreach(Materia materia in alumno.Materias)
+            {
+                Console.WriteLine("Materia: " + materia.Nombre);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
